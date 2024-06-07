e1866052250f0fa4b5f687605e3a96e7b4236326
vonwe <vonwe@pvamu.edu>
2024-06-06 23:31:21 -0500

    completed homepage and .gitignore has been reconfigured

--- who-sampled-who/src/app/home/home.component.ts
+++ who-sampled-who/src/app/home/home.component.ts
@@ -5,9 +5,19 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
+
+    <ul>
+      <li><a href="">Recents</a></li>
+      <li><a href="">Latest Hits</a></li>
+      <li><a href="">Greatest Hits</a></li>
+    </ul>
+
+    <section class="musica-works" text-center>
+    <img src="./assets/whos_who_logo.jpg" alt="record_scratch" height="300" width="300">
+    </section>
+
     <section>
       <form class="big-search" text-center>
-          <img src="./assets/whos_who_logo.jpg">
           <input type="text" placeholder="Look up by track name or lyrics">
           <button class="primary" type="button">Go</button>
       </form>
